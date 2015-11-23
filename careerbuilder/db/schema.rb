dc19dcbfcae4454ba19dc1f93684b519c6a477d5
David Pickeral <dwpickeral@gmail.com>
2015-11-23 18:55:13 -0500

    add names to users

--- careerbuilder/db/schema.rb
+++ careerbuilder/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123234620) do
+ActiveRecord::Schema.define(version: 20151123235418) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20151123234620) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "firstname"
+    t.string   "lastname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
